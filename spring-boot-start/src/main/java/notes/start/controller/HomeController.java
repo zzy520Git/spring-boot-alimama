ab46568d2f5340c4e083ab26bb7afae9538bcd3f
zhouzhongyi <zhouzhongyi.zzy@alibaba-inc.com>
2020-12-11 15:44:59 +0800

    加入一些配置

--- spring-boot-start/src/main/java/notes/start/controller/HomeController.java
+++ spring-boot-start/src/main/java/notes/start/controller/HomeController.java
@@ -1,5 +1,6 @@
 package notes.start.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
  * @author zhouzhongyi
  * @date 2020/12/11
  */
+@Slf4j
 @RestController
 @RequestMapping
 public class HomeController {
+
+    @RequestMapping("alive")
+    public Object alive() {
+        log.info("服务存活哦");
+        return "hello 中文";
+    }
 }
