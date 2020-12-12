cc2822117a59219b467432e700b63774ed59ffaf
zhouzhongyi <zhouzhongyi.zzy@alibaba-inc.com>
2020-12-12 17:15:07 +0800

    WebMvcConfig

---
+++ spring-boot-start/src/main/java/notes/start/javaconfig/WebMvcConfig.java
@@ -0,0 +1,15 @@
+package notes.start.javaconfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Description:
+ * 扩展 spring-mvc的功能，而不是全面接管
+ *
+ * @author zhouzhongyi
+ * @date 2020/12/12
+ */
+//@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+}
