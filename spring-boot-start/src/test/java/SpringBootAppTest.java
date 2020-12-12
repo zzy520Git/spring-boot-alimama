54fcee9a5021be0a5cd6570e64c0c7809f6138c2
zhouzhongyi <zhouzhongyi.zzy@alibaba-inc.com>
2020-12-12 14:47:50 +0800

    repackage

---
+++ spring-boot-start/src/test/java/SpringBootAppTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Description:
+ *
+ * @author zhouzhongyi
+ * @date 2020/12/12
+ */
+@SpringBootTest
+public class SpringBootAppTest {
+    @Test
+    public void contextLoad() {
+        System.out.println("hello");
+    }
+}
