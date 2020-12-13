5822a8b43d3df1913925344d6f185a0a97d1a8a6
zhouzhongyi <zhouzhongyi.zzy@alibaba-inc.com>
2020-12-13 16:47:28 +0800

    mybatis测试

--- spring-boot-datasource/src/main/java/notes/datasource/mysql/dao/TbBuyerDao.java
+++ spring-boot-datasource/src/main/java/notes/datasource/mysql/dao/TbBuyerDao.java
@@ -1,7 +1,12 @@
 package notes.datasource.mysql.dao;
 
 import notes.datasource.mysql.podo.TbBuyer;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
+@Repository
+//必需配这个注解，或者在Main上配MapperScan扫描
+@Mapper
 public interface TbBuyerDao {
     int deleteByPrimaryKey(Long id);
 
--- spring-boot-start/src/main/java/notes/start/controller/JDBCController.java
+++ spring-boot-start/src/main/java/notes/start/controller/JDBCController.java
@@ -1,6 +1,8 @@
 package notes.start.controller;
 
 import notes.common.AliResult;
+import notes.datasource.mysql.dao.TbBuyerDao;
+import notes.datasource.mysql.podo.TbBuyer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,10 +23,19 @@ public class JDBCController {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    @Autowired
+    private TbBuyerDao tbBuyerDao;
+
 
     @RequestMapping("/buyers")
     public AliResult buyers() {
         List<Map<String, Object>> buyers = jdbcTemplate.queryForList("select * from tb_buyer");
         return AliResult.success(buyers);
+    }
+
+    @RequestMapping("/buyerlist")
+    public AliResult buyerList() {
+        TbBuyer tbBuyer = tbBuyerDao.selectByPrimaryKey(1L);
+        return AliResult.success(tbBuyer);
     }
 }
