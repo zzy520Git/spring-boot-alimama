6d2e3eb8ffbeae5599c4887f6a812eb12c15363a
zhouzhongyi <zhouzhongyi.zzy@alibaba-inc.com>
2020-12-13 22:08:21 +0800

    Flux测试

--- spring-boot-common/src/test/java/console/tools/FluxDemo.java
+++ spring-boot-common/src/test/java/console/tools/FluxDemo.java
@@ -2,6 +2,8 @@ package console.tools;
 
 import reactor.core.publisher.Flux;
 
+import java.time.Duration;
+
 /**
  * Description:
  *
@@ -10,11 +12,30 @@ import reactor.core.publisher.Flux;
  */
 public class FluxDemo {
     public static void main(String[] args) {
-        print();
+//        print();
+        takeTimes();
+//        takeCondition();
     }
 
     public static void print() {
         Flux<Integer> range = Flux.range(1, 10);
+        range.subscribe(System.out::println);
+    }
+
+    public static void takeNum() {
+        Flux<Integer> range = Flux.range(1, 10).take(5);
+        range.subscribe(System.out::println);
+    }
+
+    public static void takeTimes() {
+        Flux<Integer> range = Flux.range(1, 1000)
+                .take(Duration.ofMillis(1));
+        range.subscribe(System.out::println);
+    }
+
+    public static void takeCondition() {
+        Flux<Integer> range = Flux.range(1, 1000)
+                .takeUntil(o -> o >= 10);
         range.subscribe(System.out::println);
     }
 
