f553197ef50d452bab8f88686b40772c12ad8e0c
zhouzhongyi <zhouzhongyi.zzy@alibaba-inc.com>
2020-12-13 15:18:10 +0800

    拦截器

---
+++ spring-boot-start/src/main/java/notes/start/interceptor/LoginInterceptor.java
@@ -0,0 +1,23 @@
+package notes.start.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Description:
+ *
+ * @author zhouzhongyi
+ * @date 2020/12/13
+ */
+@Slf4j
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.warn("登录拦截器preHandle");
+        return true;
+    }
+}
