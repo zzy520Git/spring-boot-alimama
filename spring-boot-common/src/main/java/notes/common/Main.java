7a48527b087819053d28de1825e6f1cdfd19b66d
zhouzhongyi <zhouzhongyi.zzy@alibaba-inc.com>
2021-01-17 17:54:33 +0800

    sentinel限流源码

---
+++ spring-boot-common/src/main/java/notes/common/Main.java
@@ -0,0 +1,56 @@
+package notes.common;
+
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Description:
+ *
+ * @author zhouzhongyi
+ * @date 2021/1/17
+ */
+public class Main {
+    public static void main(String[] args) {
+        testSphU();
+    }
+
+    public static void testSphU() {
+        //initFlowRules();
+        int i = 1;
+        while (i-- > 0) {
+            Entry entry = null;
+            try {
+                entry = SphU.entry("HelloWorld");
+                /*您的业务逻辑 - 开始*/
+                System.out.println("hello world");
+                /*您的业务逻辑 - 结束*/
+            } catch (BlockException e1) {
+                /*流控逻辑处理 - 开始*/
+                System.out.println("block!");
+                /*流控逻辑处理 - 结束*/
+            } finally {
+                if (entry != null) {
+                    entry.exit();
+                }
+            }
+        }
+    }
+
+    private static void initFlowRules(){
+        List<FlowRule> rules = new ArrayList<>();
+        FlowRule rule = new FlowRule();
+        rule.setResource("HelloWorld");
+        rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        // Set limit QPS to 20.
+        rule.setCount(5);
+        rules.add(rule);
+        FlowRuleManager.loadRules(rules);
+    }
+}
