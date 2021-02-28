eb73ba12b56ecace643ed42f0509ef507732498f
zhouzhongyi <zhouzhongyi.zzy@alibaba-inc.com>
2021-02-28 17:41:54 +0800

    Flux

--- spring-boot-common/src/test/java/console/tools/FluxDemo.java
+++ spring-boot-common/src/test/java/console/tools/FluxDemo.java
@@ -1,8 +1,12 @@
 package console.tools;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
 
 import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * Description:
@@ -12,9 +16,32 @@ import java.time.Duration;
  */
 public class FluxDemo {
     public static void main(String[] args) {
+        testZipWith();
+        //        testFuture();
 //        print();
-        takeTimes();
+//        takeTimes();
 //        takeCondition();
+    }
+
+    public static void testZipWith() {
+        Flux.just("a", "b")
+                .zipWith(Flux.just("c", "d"))
+                .subscribe(System.out::println);
+    }
+
+    public static void testFuture() {
+        System.out.println(Thread.currentThread().getId() +
+                Thread.currentThread().getName());
+        CompletableFuture<Integer> fu = Mono.just(1).map(t -> {
+            System.out.println(Thread.currentThread().getId() +
+                    Thread.currentThread().getName());
+            return t + 1;
+        }).map(t -> {
+            System.out.println(Thread.currentThread().getId() +
+                    Thread.currentThread().getName());
+            return t + 3;
+        }).subscribeOn(Schedulers.elastic()).toFuture();
+        System.out.println(fu.join());
     }
 
     public static void print() {
